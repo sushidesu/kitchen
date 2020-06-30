612847df58dc3f0f8287e974493d7d69cbdad9f9
sushidesu <hugemerge@gmail.com>
2020-06-30 07:30:09 +0000

    Use typescript

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const Index = () => (
+    <div className="container">
+      <Head>
+        <title>kitchen by sushidesu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  )
+
+export default Index
