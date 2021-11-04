595b496343552ffd4609c6a50c61f0b25390bb2e
sushidesu <hugemerge@gmail.com>
2021-11-04 13:34:09 +0900

    create tool config file

---
+++ src/tools/ConvertEnv/tool.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  slug: "convert-env",
+  title: "Convert Env",
+  description: `JSONのデータをenvのフォーマットに変換します`,
+}
---
+++ src/tools/CountLength/tool.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  slug: "count-length",
+  title: "Count Length",
+  description: `文字数をカウントします`,
+}
