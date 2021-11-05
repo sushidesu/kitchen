b8084370b62e40a076ebdb9cb360c99d51a35c4e
sushidesu <hugemerge@gmail.com>
2021-11-05 19:48:08 +0900

    toggle color mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,27 @@
 import React from "react"
-import { Heading, Flex, Spacer, Box, Switch } from "@chakra-ui/react"
+import {
+  useColorMode,
+  Heading,
+  Flex,
+  Spacer,
+  Box,
+  Switch,
+} from "@chakra-ui/react"
 import { Container } from "./Container"
 
-export const Header = (): JSX.Element => (
-  <Container>
-    <Flex height="full" alignItems="center">
-      <Box>
-        <Heading size="lg">Kitchen 🍳</Heading>
-      </Box>
-      <Spacer />
-      <Box>
-        <Switch />
-      </Box>
-    </Flex>
-  </Container>
-)
+export const Header = (): JSX.Element => {
+  const { toggleColorMode, colorMode } = useColorMode()
+  return (
+    <Container>
+      <Flex height="full" alignItems="center">
+        <Box>
+          <Heading size="lg">Kitchen 🍳</Heading>
+        </Box>
+        <Spacer />
+        <Box>
+          <Switch isChecked={colorMode === "dark"} onChange={toggleColorMode} />
+        </Box>
+      </Flex>
+    </Container>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 import "../../src/styles/global.css"
+import theme from "../styles/theme"
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
