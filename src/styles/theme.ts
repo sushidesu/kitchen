ee771e7b639b4047adab7ef3bdd2186df93664f9
sushidesu <hugemerge@gmail.com>
2021-11-07 18:34:58 +0900

    fix font-family

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,6 +4,13 @@ const config: ThemeConfig = {
   initialColorMode: "light",
   useSystemColorMode: false,
 }
-const theme = extendTheme({ config })
+const theme = extendTheme({
+  config,
+  fonts: {
+    // ref: https://zenn.dev
+    body: `-apple-system,"BlinkMacSystemFont","Hiragino Kaku Gothic ProN","Hiragino Sans",Meiryo,sans-serif,"Segoe UI Emoji"`,
+    heading: `-apple-system,"BlinkMacSystemFont","Hiragino Kaku Gothic ProN","Hiragino Sans",Meiryo,sans-serif,"Segoe UI Emoji"`,
+  },
+})
 
 export default theme
