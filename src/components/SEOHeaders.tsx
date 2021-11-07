f143e94bd66d8665273476ef795660d901db5e6a
sushidesu <hugemerge@gmail.com>
2021-11-07 13:51:24 +0900

    試しにogpを表示

--- src/components/SEOHeaders.tsx
+++ src/components/SEOHeaders.tsx
@@ -9,7 +9,14 @@ type SEOHeadersProps = {
 
 export function SEOHeaders(props: SEOHeadersProps): JSX.Element {
   const { title = "", titleType = "top" } = props
+
   const APP_NAME = "Kitchen"
+  const APP_ROOT_URL =
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:3000"
+      : process.env.NEXT_PUBLIC_APP_ROOT_URL
+  const OG_IMAGE_URL = `${APP_ROOT_URL}/ogp.png`
+
   const page_title: { [key in TitleType]: string } = {
     top: APP_NAME,
     page: `${title} - ${APP_NAME}`,
@@ -19,6 +26,16 @@ export function SEOHeaders(props: SEOHeadersProps): JSX.Element {
   return (
     <NextHeadSeo
       title={page_title[titleType]}
+      canonical={APP_ROOT_URL}
+      twitter={{
+        card: "summary_large_image",
+      }}
+      og={{
+        siteName: APP_NAME,
+        type: "website",
+        url: APP_ROOT_URL,
+        image: OG_IMAGE_URL,
+      }}
       customLinkTags={[
         {
           key: "favicon",
