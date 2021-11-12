569d1e80ff94b7d0bf57513082a2984eac65b113
sushidesu <hugemerge@gmail.com>
2021-11-12 10:06:39 +0900

    blob+コンポーネントをセットで返すhooks

--- src/components/Sketch/Sketch.tsx
+++ src/components/Sketch/Sketch.tsx
@@ -1,69 +1,43 @@
-import { useState, useRef, useCallback, useEffect } from "react"
+import {
+  forwardRef,
+  ForwardRefRenderFunction,
+  PointerEventHandler,
+} from "react"
 import styles from "./Sketch.module.css"
 
 export type SketchProps = {
   width?: string
   height?: string
-  color?: string
+  onPointerUp?: PointerEventHandler<HTMLCanvasElement>
+  onPointerDown?: PointerEventHandler<HTMLCanvasElement>
+  onPointerMove?: PointerEventHandler<HTMLCanvasElement>
 }
 
-export function Sketch(props: SketchProps): JSX.Element {
-  const { width = "200px", height = "200px", color = "#000" } = props
-
-  const [drag, setDrag] = useState(false)
-  const [fromX, setFromX] = useState(0)
-  const [fromY, setFromY] = useState(0)
-
-  const canvas = useRef<HTMLCanvasElement>(null)
-  const ctx = canvas.current?.getContext("2d")
-
-  const offset = useCallback(
-    (
-      clientX: number,
-      clientY: number,
-      canvasElement: HTMLCanvasElement
-    ): [number, number] => {
-      const rect = canvasElement.getBoundingClientRect()
-      return [clientX - rect.left, clientY - rect.top]
-    },
-    []
-  )
-
+const SketchWithoutRef: ForwardRefRenderFunction<
+  HTMLCanvasElement,
+  SketchProps
+> = (props, ref) => {
+  const {
+    width = "200px",
+    height = "200px",
+    onPointerUp,
+    onPointerDown,
+    onPointerMove,
+  } = props
   return (
-    <canvas
-      className={styles.wrapper}
-      ref={canvas}
-      width={width}
-      height={height}
-      style={{ border: "1px solid #333" }}
-      onPointerDown={(e) => {
-        if (!canvas.current) {
-          return
-        }
-
-        setDrag(true)
-        // from
-        const [x, y] = offset(e.clientX, e.clientY, canvas.current)
-        setFromX(x)
-        setFromY(y)
-      }}
-      onPointerUp={() => {
-        setDrag(false)
-      }}
-      onPointerMove={(e) => {
-        if (!drag || !ctx || !canvas.current) {
-          return
-        }
-        // paint
-        ctx.beginPath()
-        ctx.strokeStyle = color
-        ctx.moveTo(fromX, fromY)
-        const [x, y] = offset(e.clientX, e.clientY, canvas.current)
-        ctx.lineTo(x, y)
-        ctx.stroke()
-        setFromX(x)
-        setFromY(y)
-      }}
-    />
+    <div>
+      <canvas
+        className={styles.wrapper}
+        ref={ref}
+        width={width}
+        height={height}
+        style={{ border: "1px solid #333" }}
+        onPointerUp={onPointerUp}
+        onPointerDown={onPointerDown}
+        onPointerMove={onPointerMove}
+      />
+    </div>
   )
 }
+
+export const Sketch = forwardRef(SketchWithoutRef)
--- src/pages/what-is-this.tsx
+++ src/pages/what-is-this.tsx
@@ -14,34 +14,41 @@ import { Layout } from "../components/Layout"
 import { SEOHeaders } from "../components/SEOHeaders"
 import { useInput } from "../hooks/useInput"
 import { useLoading } from "../hooks/useLoading"
-import { Sketch } from "../components/Sketch"
+import { useSketch } from "../components/Sketch/useSketch"
 
 function WhatIsThisPage(): JSX.Element {
   const [name, handleChangeName, , resetName] = useInput()
   const [body, handleChangeBody, , resetBody] = useInput()
   const [loading, setLoading] = useLoading()
 
+  const [getBlob, renderCanvas] = useSketch({ color: "#000" })
+
   const sendMessage = async () => {
     setLoading(true)
-    const result = await fetch(process.env.NEXT_PUBLIC_SEND_MESSAGE_URL ?? "", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      mode: "cors",
-      body: JSON.stringify({
-        message: `お名前: ${name}
-要望・感想など:
-${body}
-`,
-      }),
-    })
-
-    if (result.ok) {
-      resetName()
-      resetBody()
-    }
-    window.alert(result.ok ? "OK" : "Failed")
+    const blob = await getBlob()
+    const img = document.createElement("img")
+    const url = URL.createObjectURL(blob)
+    img.src = url
+    console.log(img)
+    //    const result = await fetch(process.env.NEXT_PUBLIC_SEND_MESSAGE_URL ?? "", {
+    //      method: "POST",
+    //      headers: {
+    //        "Content-Type": "application/json",
+    //      },
+    //      mode: "cors",
+    //      body: JSON.stringify({
+    //        message: `お名前: ${name}
+    //要望・感想など:
+    //${body}
+    //`,
+    //      }),
+    //    })
+    //
+    //    if (result.ok) {
+    //      resetName()
+    //      resetBody()
+    //    }
+    //    window.alert(result.ok ? "OK" : "Failed")
     setLoading(false)
   }
   return (
@@ -64,9 +71,7 @@ ${body}
               <FormLabel>要望・感想など</FormLabel>
               <Textarea value={body} onChange={handleChangeBody} rows={8} />
             </FormControl>
-            <div>
-              <Sketch color={"#000"} />
-            </div>
+            {renderCanvas()}
           </Stack>
           <ButtonGroup justifyContent="center">
             <Button
